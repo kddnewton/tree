d5c50fbb6666d6c29edfa92b6dce801b539b6188
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-30 13:26:22 -0500

    Add rust

--- tree.rs
+++ tree.rs
@@ -1,5 +1,62 @@
 use std::env;
+use std::fs;
+use std::io;
 
-fn main() {
+struct Counts {
+  dirs: i32,
+  files: i32
+}
+
+fn walk(dir: &str, prefix: &str, counts: &mut Counts) -> io::Result<()> {
+  let mut paths: Vec<_> = fs::read_dir(dir)?.map(|entry| entry.unwrap().path()).collect();
+  let mut index = paths.len();
+
+  paths.sort_by(|a, b| {
+    let aname = a.file_name().unwrap().to_str().unwrap();
+    let bname = b.file_name().unwrap().to_str().unwrap();
+    aname.cmp(bname)
+  });
+
+  for path in paths {
+    let name = path.file_name().unwrap().to_str().unwrap();
+
+    if name.starts_with(".") {
+      index -= 1;
+      continue;
+    }
+
+    if path.is_dir() {
+      counts.dirs += 1;
+    } else {
+      counts.files += 1;
+    }
+
+    if index == 1 {
+      println!("{}└── {}", prefix, name);
+      if path.is_dir() {
+        walk(&format!("{}/{}", dir, name), &format!("{}    ", prefix), counts)?;
+      }
+    } else {
+      println!("{}├── {}", prefix, name);
+      if path.is_dir() {
+        walk(&format!("{}/{}", dir, name), &format!("{}│   ", prefix), counts)?;
+      }
+    }
+
+    index -= 1;
+  }
+
+  Ok(())
+}
+
+fn main() -> io::Result<()> {
   let dir = env::args().nth(1).unwrap_or(".".to_string());
+  println!("{}", dir);
+
+  let mut counts = Counts { dirs: 0, files: 0 };
+  walk(&dir, "", &mut counts)?;
+
+  println!("\n{} directories, {} files", counts.dirs, counts.files);
+
+  Ok(())
 }
