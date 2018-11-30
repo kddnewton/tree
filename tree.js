0a5268755363f62d25e45e8e9b96ec6ef5e133ed
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-30 13:42:55 -0500

    Use newer syntax for javascript

--- tree.js
+++ tree.js
@@ -1,61 +1,39 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-function Tree() {
-  this.dirCount = 0;
-  this.fileCount = 0;
-}
+const walk = (directory, prefix, counts) => {
+  const filepaths = fs.readdirSync(directory);
 
-Tree.prototype._register = function (filepath) {
-  if (fs.lstatSync(filepath).isDirectory()) {
-    this.dirCount += 1;
-  } else {
-    this.fileCount += 1;
-  }
-};
-
-Tree.prototype.walk = function (directory, prefix) {
-  var _this = this;
-  var filepaths = fs.readdirSync(directory);
-
-  filepaths.forEach(function (filepath, index) {
-    var absolute;
-    var isDirectory;
-
-    if (filepath.charAt(0) == '.') {
+  filepaths.forEach((filepath, index) => {
+    if (filepath.charAt(0) == ".") {
       return;
     }
-    absolute = path.join(directory, filepath);
-    _this._register(absolute);
 
-    isDirectory = fs.lstatSync(absolute).isDirectory();
+    const absolute = path.join(directory, filepath);
+    const isDirectory = fs.lstatSync(absolute).isDirectory();
+
+    counts[isDirectory ? "dirs" : "files"] += 1;
 
     if (index == filepaths.length - 1) {
-      console.log(prefix + '└── ' + filepath);
+      console.log(`${prefix}└── ${filepath}`);
       if (isDirectory) {
-        _this.walk(absolute, prefix + '    ');
+        walk(absolute, `${prefix}    `, counts);
       }
     } else {
-      console.log(prefix + '├── ' + filepath);
+      console.log(`${prefix}├── ${filepath}`);
       if (isDirectory) {
-        _this.walk(absolute, prefix + '│   ');
+        walk(absolute, `${prefix}│   `, counts);
       }
     }
   });
 };
 
-Tree.prototype.summary = function () {
-  return this.dirCount.toString() + ' directories, ' + this.fileCount + ' files';
-}
+const directory = process.argv[2] || ".";
+const counts = { dirs: 0, files: 0 };
 
-var directory = '.';
-if (process.argv.length > 2) {
-  directory = process.argv[2];
-}
 console.log(directory);
+walk(directory, "", counts);
 
-var tree = new Tree();
-tree.walk(directory, '');
-console.log("\n" + tree.summary());
+console.log(`\n${counts.dirs} directories, ${counts.files} files`);
